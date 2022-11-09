d9894dd43febd8e5485534a2549aa6fdd1c80eb2
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-11-09 17:59:00 +0700

    Menambahkan method main pada class program

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -117,11 +117,87 @@ namespace single_linked_list
             else
                 return false;
         }
-    }
-    class program
-    {
-        static void Main(string[] args)
+        class program
         {
+            static void Main(string[] args)
+            {
+                List obj = new List();
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("\nMenu\n");
+                        Console.WriteLine("1. Menambah data kedalam list");
+                        Console.WriteLine("2. MEnghapus data dari dalam list");
+                        Console.WriteLine("3. Melihat semua data didalam list");
+                        Console.WriteLine("4. MEncari sebuah data didalam list");
+                        Console.WriteLine("5. Exit");
+                        Console.WriteLine("\nMasukkan pilihan anda (1-5): ");
+                        char ch = Convert.ToChar(Console.ReadLine());
+                        switch (ch)
+                        {
+                            case '1':
+                                {
+                                    obj.addNode();
+                                }
+                                break;
+                            case '2':
+                                {
+                                    if (obj.listEmpty())
+                                    {
+                                        Console.WriteLine("\nList kosong");
+                                        break;
+                                    }
+                                    Console.Write("\nMasukkan nomor mahasiswa yang akan dihapus: ");
+                                    int nim = Convert.ToInt32(Console.ReadLine());
+                                    if (obj.delNode(nim) == false)
+                                        Console.WriteLine("\nData tidak ditemukan.");
+                                    else
+                                        Console.WriteLine("Data dengan nomor Mahasiswa " + nim + " dihapus");
+                                }
+                                break ;
+                            case '3':
+                                {
+                                    obj.traverse();
+                                }
+                                break;
+                            case '4':
+                                {
+                                    if(obj.listEmpty() == true)
+                                    {
+                                        Console.WriteLine("\nList kosong");
+                                        break ;
+                                    }
+                                    Node previous, current;
+                                    previous = current = null;
+                                    Console.Write("\nMasukkan nomor mahasiswa yang akan dicari: ");
+                                    int num = Convert.ToInt32(Console.ReadLine());
+                                    if (obj.Search(num, ref previous, ref current) == false)
+                                        Console.WriteLine("\nData tidak ditemukan.");
+                                    else
+                                    {
+                                        Console.WriteLine("\nData ketemu");
+                                        Console.WriteLine("\nNomor Mahasiswa: " + current.noMhs);
+                                        Console.WriteLine("\nNama Mahasiswa: " + current.nama);
+                                    }
+                                }
+                                break;
+                            case '5':
+                                return;
+                            default:
+                                {
+                                    Console.WriteLine("\nPilihan tidak valid");
+                                    break;
+                                }
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("\nCheck nilai yang anda masukkan.");
+                    }
+                }
+            }
         }
     }
+   
 }
