1fb151942fe238a0b25da992daaacd373073a965
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-11-09 17:07:13 +0700

    Menemukan lokasi node baru

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -27,6 +27,41 @@ namespace single_linked_list
             string nm;
             Console.Write("\nMasukkan nomer Mahasiswa: ");
             nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru =  new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if(START == null || nim <= START.noMhs)/*Node ditambahkan sebagai node pertama*/
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer Mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+
+            /*Menemukan lokasi node baru didalam list*/
+
+            Node previous, current;
+            previous = START;
+            current = START;
+
+            while((current != null) && (nim >= current.noMhs))
+            {
+                if(nim == current.noMhs)
+                {
+                    Console.WriteLine("\nNomer Mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                previous = current;
+                current = current.next;
+            }
+            
         }
     }
     class program
